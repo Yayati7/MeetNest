ff7e46f68ffcc0b1fc99e3f181148f826ab1bfd4
Yayati7 <singhpsby@gmail.com>
2025-12-30 08:40:43 +0530

    Deploying Frontend

--- frontend/src/utils/environment.js
+++ frontend/src/utils/environment.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://meetnestbackend-3e0n.onrender.com" :
 
     "http://localhost:8000"
 
